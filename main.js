684ffc67a3dbd1ae2f638ffdf075a1f91469307e
AnnaNowak96 <jankowska.anna-96@wp.pl.noreply.github.com>
2024-03-12 15:02:11 +0100

    add html, object for gameboard and players

---
+++ main.js
@@ -0,0 +1,10 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+}
+
+function Players() {
+    const playerOne = "Player One";
+    const playerTwo = "Player Two";
+}
