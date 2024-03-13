0dfb325beb16a51e05e51550d3d0d059bb070e1f
AnnaNowak96 <jankowska.anna-96@wp.pl.noreply.github.com>
2024-03-13 13:59:48 +0100

    add logic to create cells and select only one and put there a token

--- main.js
+++ main.js
@@ -1,10 +1,54 @@
+function Cell() {
+    return " ";
+}
+
+const playerOne = {name: "Player One", token: "X"};
+const playerTwo = {name: "Player Two", token: "O"};
+
+let board;
+
 function Gameboard() {
     const rows = 3;
     const columns = 3;
-    const board = [];
+    board = [];
+
+    for (i  = 0; i < rows; i++) {
+        board[i] = [];
+        for (j = 0; j < columns; j++) {
+            board[i].push(Cell());
+            console.log(board);
+        }
+    }
+    return board;
 }
 
-function Players() {
-    const playerOne = "Player One";
-    const playerTwo = "Player Two";
+
+function dropToken(rowIndex, colIndex, player) {
+    if (board[rowIndex][colIndex] === " ") {
+        board[rowIndex][colIndex] = player.token;
+        console.log(board);
+        return true; 
+    } else {
+        console.log("Cell is already occupied");
+        return false; 
 }
+}
+
+function CheckWinner() {
+    
+
+}
+
+function CheckActivePlayer() {
+
+}
+
+function GamePlay() {
+    Gameboard();
+    dropToken(1, 1, playerOne);
+    dropToken(1, 1, playerTwo);
+
+}
+
+GamePlay();
+
